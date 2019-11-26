a65229b61c422d25d4d3fbf354171c6607bfa858
tIAgOfErrE <https://github.com/tIAgOfErrE/OneSystem.git>
2019-11-26 11:39:37 -0300

    Incluindo rotas

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ app.get("login", function(req, res){
     res.send("Fazer tela de login");
 })
 
-
-
 app.listen(8081, function(){
     console.log("Servidor rodando na url http://localhost:8081");
 });
